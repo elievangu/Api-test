6904c17e3effa2b61eafab5793332775f91ecb6b
Elie Vangu <vangu.elie@gmail.com>
2019-01-09 16:52:30 +0100

    API request with Axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//npm import
+import React from 'react';
+import axios from 'axios';
 
-class App extends Component {
+//local import
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`https://swapi.co/api/people/1/.json`)
+    .then(
+      res => {
+        const datas = res.data;
+        this.setState({posts:datas});
+        console.log(this.state.posts)
+      }
+    )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.posts.name}</h1>
       </div>
-    );
+    )
   }
 }
 
